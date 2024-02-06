19cf780ec798dfb2c91e21641046a382d5da1124
surendratiwari3 <surendratiwari3@gmail.com>
2024-02-06 08:51:42 +0530

    sonarqube: fix logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -11,7 +11,6 @@ var ApplicationLogger Logger = &DefaultLogger{}
 
 func (l *DefaultLogger) Debug(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Info(args ...interface{}) {
@@ -20,12 +19,10 @@ func (l *DefaultLogger) Info(args ...interface{}) {
 
 func (l *DefaultLogger) Warn(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Warning(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Error(args ...interface{}) {
@@ -34,80 +31,64 @@ func (l *DefaultLogger) Error(args ...interface{}) {
 
 func (l *DefaultLogger) Fatal(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Panic(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Debugln(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Infoln(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Warnln(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Warningln(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Errorln(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Fatalln(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Panicln(args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Debugf(format string, args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Infof(format string, args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Warnf(format string, args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Warningf(format string, args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Errorf(format string, args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Fatalf(format string, args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (l *DefaultLogger) Panicf(format string, args ...interface{}) {
 	//TODO implement me
-	panic("implement me")
 }
